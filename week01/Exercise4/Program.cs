c0248f1d05cbf6efc8c3d628dc5c6013c177051b
Dalin Briggs <dtox568@byui.edu>
2025-01-09 11:06:11 -0700

    Finished exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,41 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
 
         List<int> numbers = new List<int>();
         int userNumber = -1;
 
-        while(userNumber != 0){
-             Console.Write("Enter your numbers. (enter 0 to stop):  ");
-        while(true)
+        while(userNumber != 0)
         {
-            userNumber = int.Parse(Console.ReadLine());
+             Console.Write("Enter your numbers. (enter 0 to stop):  ");
+        
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
 
-            if(userNumber == 0)
+            if(userNumber != 0)
             {
-                break;
+                numbers.Add(userNumber);
             }
-            numbers.Add(userNumber);
         }
+        
 
         if(numbers.Count == 0) // in case 0 is written at the very start.
         {
-        Console.WriteLine("No numbers were entered.");
-        return;
+            Console.WriteLine("No numbers were entered. Have a nice day!");
+            return;
         }
-        }
+    
 
 
-       
+       //Adding the sum
         int sum = 0;
-        foreach (int num in numbers)
+        foreach (int number in numbers)
         {
-            sum += num;
+            sum += number;
         }
+        Console.WriteLine($"The sum of the numbers is {sum}.");
+
+        //Computing average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of the numbers is {average}");
+
+        //Getting max
+        int max = numbers[0];
+
+        foreach(int number in numbers)
+        {
+            if(number > max)
+            {
+                // going through list and finding a higher number will then make that the max
+                max = number; 
+            }
+        }
+        Console.WriteLine($"The highest number is {max}.");
     }
 }
