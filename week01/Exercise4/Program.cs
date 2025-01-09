3e03b04e07030fc6db0477dd628afe74c404c7c0
Dalin Briggs <dtox568@byui.edu>
2025-01-09 10:37:02 -0700

    Working on exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        while(userNumber != 0){
+             Console.Write("Enter your numbers. (enter 0 to stop):  ");
+        while(true)
+        {
+            userNumber = int.Parse(Console.ReadLine());
+
+            if(userNumber == 0)
+            {
+                break;
+            }
+            numbers.Add(userNumber);
+        }
+
+        if(numbers.Count == 0) // in case 0 is written at the very start.
+        {
+        Console.WriteLine("No numbers were entered.");
+        return;
+        }
+        }
+
+
+       
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
     }
 }
