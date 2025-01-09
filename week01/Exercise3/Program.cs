5c4e3ba2406c4873ca5968ef657723259d4d9b00
Dalin Briggs <dtox568@byui.edu>
2025-01-09 10:06:28 -0700

    Finished up with exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
+       
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random random = new Random();
+
+
+        int targetNumber = random.Next(1,101); //Number randomly chosen from 1 to 100
+        int userGuesses = 0;
+
+        Console.WriteLine("I have chosen a number randomly from 1 to 100. Try and find it!");
+
+        while (userGuesses != targetNumber)
+        {
+                Console.Write("Enter a number: ");
+
+                if (int.TryParse(Console.ReadLine(), out userGuesses))
+                {
+                    if (userGuesses > targetNumber)
+                    {
+                        Console.WriteLine("Too high!");
+                    }
+                    
+                    else 
+                    {
+                    Console.WriteLine("Too low!");
+                    }
+            }
+           
+        }
+
+         Console.WriteLine($"You guessed it! Way to be!");
     }
 }
