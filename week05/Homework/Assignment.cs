04eb0bdb2704987c8943a7eca4e79f50f08a10e0
Dalin Briggs <dtox568@byui.edu>
2025-02-03 17:09:35 -0700

    Starting Week 05 Learning Activity

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    /*We will provide Getters for our private variables so they can be accessed 
+    later both outside the class and same as derived classes*/
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}
