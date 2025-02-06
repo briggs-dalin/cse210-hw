ddedc01b1336d732e258bee1c8ab7a5c8ba8a0c0
Dalin Briggs <dtox568@byui.edu>
2025-02-06 13:42:36 -0700

    Came back to do a little more on Listing Activity

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -10,7 +10,9 @@ public class ListingActivity : Activity
         "What are personal strengths of yours?",
         "Who are people that you have helped this week?",
         "When have you felt the Holy Ghost this month?",
-        "Who are some of your personal heroes?"
+        "Who are some of your personal heroes?",
+        "How has someone helped you in the past month?",
+        "What qualities do you admire in yourself?"
     };
     public ListingActivity()
     {
@@ -18,17 +20,31 @@ public class ListingActivity : Activity
         SetDescription("This activity will help you reflect on the good things you have in life. Set a timer and list as many as you can in that time limit.");
     }
 
-    public void GeneratePrompt()
+    public string GeneratePrompt()
     {
         Random random = new Random();
         int index = random.Next(_prompts.Count);//Select random prompt
         Console.WriteLine("Your prompt is:");
-        Console.WriteLine(_prompts[index]);//Display prompt
+        return _prompts[index];//Display prompt
     }
 
     public void PromptListing()
     {
+        string prompt = GeneratePrompt();
+        Console.WriteLine();
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        Console.WriteLine($"---{prompt}---");
+        
+        //Prompt user to press any key when ready to begin
+        Console.WriteLine("When you are ready, press any key to start.");
+        Console.ReadKey();//Waits for user to press a key
 
+        Console.WriteLine("\nYou may begin in:");
+        GenerateCountdownTimer(5);
+        Console.WriteLine();//Adding space to look nicer
+
+
+        
     }
 
 }
