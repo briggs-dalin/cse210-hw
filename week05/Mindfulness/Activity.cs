a28b243f7e934d7221650ea4babccb179449a744
Dalin Briggs <dtox568@byui.edu>
2025-02-06 08:29:21 -0700

    Starting on Activity Class

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -10,7 +10,9 @@ public class Activity
 
     public Activity()
     {
-      
+      _name = "";
+      _description = "";
+      _duration = 12;
     }
 
     public void SetName(string name)
@@ -25,12 +27,32 @@ public class Activity
 
     public void ShowDetails()
     {
+        Console.WriteLine($"Welcome to the {_name}");
+        Console.WriteLine();
+        Console.WriteLine(_description);
+    }
 
+    public void AskDuration()
+    {
+        Console.WriteLine();
+        Console.Write("How long would you like the activity to be? (In seconds)");
+        string durationString = Console.ReadLine();
+        SetDuration(Convert.ToInt32(durationString));
+    }
+
+    public void SetDuration(int duration)
+    {
+        _duration = duration;
+    }
+
+    public int GetDuraction()
+    {
+        return _duration;
     }
 
     public void StartingMessage()
     {
-
+        
     }
 
     public void EndingMessage()
@@ -40,7 +62,7 @@ public class Activity
 
     public void ShowAnimation()
     {
-
+        
     }
 
     public void ShowCountdown()
