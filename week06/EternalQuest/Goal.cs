ed34fccfc96e3f5f49f5576b184b047dc98d362c
Dalin Briggs <dtox568@byui.edu>
2025-02-13 18:18:07 -0700

    Working on project. Did most of the stuff in Goal class

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,13 +1,23 @@
 
-
+using System;
 public class Goal
 {
     private string _name;
     private string _description;
 
-    private string _points;
+    private int _points;
 
-    public Goal(string name, string description, string points)
+    protected bool _status;
+
+    public Goal()
+    {
+        _name = "Test Name";
+        _description = "Test Description";
+        _points = 50;
+        _status = false;
+    }
+
+    public Goal(string name, string description, int points)
     {
         _name = name;
         _description = description;
@@ -15,11 +25,25 @@ public class Goal
 
     }
 
+    //Method to prompt user for goal details and create said goal
     protected void CreateBaseGoal()
     {
-        
+        Console.WriteLine("What is the name of your goal? ");
+        _name = Console.ReadLine();
+
+        Console.WriteLine("What is a short description of it? ");
+        _description = Console.ReadLine();
+
+        Console.Write("What is the amount of points for this goal? ");
+        //Convert the int into a string
+        string stringPoints = (Console.ReadLine());
+        _points = Convert.ToInt32(stringPoints);
+
+        _status = false; //Goal starts out not complete
+
     }
 
+    //Virtual method for saving goal details to be used for other goal classes
     public virtual string SaveGoal()
     {
         string line = "";
